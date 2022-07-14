41f60342380f229a49207a7de9b0bc8c4c500bd4
lakshmigayathriponnala <106177175+lakshmigayathriponnala@users.noreply.github.com>
2022-07-14 13:15:51 +0530

    Add files via upload

---
+++ UorL.java
@@ -0,0 +1,15 @@
+public class UorL{
+  public static void main(String args[])
+  {
+    System.out.println("Checking for character");
+    char val='g';
+    if(Character.isUpperCase(val))
+    {
+     System.out.println("character " +val+ " is Uppercase");
+    }
+    else
+    {
+     System.out.println("character " +val+ " is Lowercase");
+    }
+  }
+}
