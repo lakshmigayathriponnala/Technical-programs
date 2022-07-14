f48591ac83948b860645e25ac116ac65199be97e
lakshmigayathriponnala <106177175+lakshmigayathriponnala@users.noreply.github.com>
2022-07-14 12:33:56 +0530

    Add files via upload

--- BooleanOperation.java
+++ BooleanOperation.java
@@ -1,20 +1,42 @@
-//JFM1T3_Assignment5:
-/*
-1.Write a program to initialize a boolean variable and print it's opposite value on console.
+/*  JFM1T2_Assignment3:
 
-  Sample Input:true
-
-  Expected Output:false
+     Print the results for the following statements considering x = true and y = false
+             1. x&&y
+             2. x||y
+             3. !x
+             4. !y
+     Prompt the user input from the terminal.
+             
+     Sample Input:
+     Enter x as boolean value(true/false): 
+     true
+     Enter y as boolean value(true/false): 
+     false
+     
+     Expected Output:
+     x&&y is: false
+     x||y is: true
+     !x is: false
+     !y is: true
 
 */
+
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
 public class BooleanOperation {
 
-//Define main method
-  public static void main(String args[]){
-    boolean x=false;
-    System.out.println(!(x));
-  
+public static void main(String args[]){
+
+                   Scanner sc = new Scanner(System.in); 
+                   System.out.print("Enter the Value of x as boolean : "); 
+                    boolean x=sc.nextBoolean(); 
+                   System.out.print("Enter the Value of y as boolean : "); 
+                    boolean y=sc.nextBoolean();
+                   System.out.println("x && y is : "+( x && y));
+                    System.out.println("x || y is : "+( x || y));
+                    System.out.println("!y is : "+(!y));
+                   System.out.println("!x is : "+(!x));
+                    
+    }
   }
-
-
-}
