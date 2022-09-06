367b82198e23298d5c3ea03a5a462736983c022f
lakshmigayathriponnala <106177175+lakshmigayathriponnala@users.noreply.github.com>
2022-09-06 09:57:23 +0530

    Add files via upload

---
+++ AnimalInheritance.java
@@ -0,0 +1,56 @@
+/*   JFM1T10_Assignment2:
+
+     Create a class Animal that has a method makeSound(). 
+     Then create two subclasses of Animal class: Horse and Cat that extends Animal class and 
+     provide their specific implementation of makeSound() method.
+
+     Sample Output:
+     The animal makes a sound
+     The Horse says: wee wee
+     The Cat says: meow meow
+
+*/
+class Animal
+  {
+    //Add Animal class and Create Sound() method 
+    public void makeSound()
+    {
+      System.out.println("the animal makes a sound");
+    }
+  }
+
+//Add Horse class that extends Animal class
+class Horse extends Animal
+  {
+    public void makeSound()
+    {
+      System.out.println("the horse says:wee wee");
+    }
+  }
+//Add Cat class that extends Animal class
+class Cat extends Animal
+  {
+    public void makeSound()
+    {
+      System.out.println("the cat says:meow meow");
+    }
+  }
+
+
+
+class  AnimalInheritance {
+ 
+//Add main method here
+  public static void main(String args[])
+  {
+    //Create all 3 Animal objects and add to an array 
+    Animal a=new Animal();
+    Horse h=new Horse();
+    Cat c=new Cat();
+    a.makeSound();
+    h.makeSound();
+    c.makeSound();
+  }
+
+}
+
---
+++ ConstructorChaining.java
@@ -0,0 +1,48 @@
+/* JFM1T10_Assignment1:
+
+   Write a program to demonstrate Constructor chaining in Java 
+   Add atleast 3 constructors
+
+   Sample Output:
+   Parent class default constructor
+   Child class default constructor
+   Parent class one-argument constructor
+   Child class one-argument constructor
+
+*/
+class Parent
+{
+  Parent()
+  {
+    System.out.println("Parent class default constructor");
+  }
+  Parent(int a)
+  {
+    System.out.println("Parent class  one argument constructor");
+  }
+}
+class Child 
+{
+     Child()
+  {
+    System.out.println("Child class default constructor");
+  }
+  Child(int a)
+  {
+    System.out.println("Child class  one argument constructor");
+  }
+}
+
+class ConstructorChaining 
+{
+
+  public static void main (String args[])
+  {
+    Parent p=new Parent();
+    Child c=new Child();
+    Parent p1=new Parent(1);
+    Child c1=new Child(1);
+  }
+
+}
+
